ebb3e688272df29880c0652c5f06a29f5df45099
andres.rpenuela <andresruizpenuela@gmail.com>
2025-07-29 18:45:31 +0200

    [PRJ000001] feat: crear vista de calculadora de creative-tim.com e integrarla en la app
    - Se crea la vista de la calculadora de https://www.creative-tim.com/twcomponents/component/calculator-ui
    - Se añade la ruta correspondiente en el router
    - Se carga el template de la calculadora en app.html
    - Se configura alias path para acceder al feature (servicios, componentes, etc.)

--- PRJ00001/zoneless-calculator/src/app/app.routes.ts
+++ PRJ00001/zoneless-calculator/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'calculator',
+    loadComponent: () => import("@calculator/layouts/calculator-layout/calculator-layout.component")
+  },
+  {
+    path: '**',
+    redirectTo: 'calculator'
+  }
+];
---
+++ PRJ00001/zoneless-calculator/src/app/calculator/layouts/calculator-layout/calculator-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator-layout',
+  templateUrl: './calculator-layout.component.html',
+  styles: ''
+})
+export default class CalculatorLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
