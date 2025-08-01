693cebe320f872c243de076b28d652e56b33ba0a
andres.rpenuela <andresruizpenuela@gmail.com>
2025-08-01 22:49:53 +0200

    [PRJ000002] feat: se añade test sobre clases y atributos html

--- PRJ00001/zoneless-calculator/src/app/app.spec.ts
+++ PRJ00001/zoneless-calculator/src/app/app.spec.ts
@@ -58,4 +58,46 @@ describe('App', () => {
     expect(compiled.querySelector('router-outlet')).not.toBeNull();
   });
 
+  // coprobar el div que contega el rotuer-outlet (hace obsoleta la anterior)
+  it('should render router-outlet wrapper with css classes', () => {
+
+    const divElement = compiled.querySelector('div'); // obtiene el primer div
+    // console.log(divElement);
+    // console.log(divElement?.className);
+    // console.log(divElement?.classList);
+
+    const expectedClasses : string[] = 'min-w-screen min-h-screen bg-slate-600 flex items-center justify-center px-5 py-5'.split(' ');
+
+    expect(divElement).not.toBeNull();
+    // esto comprueba que el div tenga las clases esperadas estricamente en el orden esperado
+    //expect(divElement?.classList.value).toBe(expectedClasses);
+    // esto comprueba que el div tenga las clases esperadas sin importar el orden, pero si se añade falla
+    // divElement?.classList.forEach((className) => {
+    //   expect(expectedClasses).toContain(className);
+    // });
+
+    // si hay clases de mas en el div, no falla, minimo debe tener las definidas en expectedClasses
+    const divClasses = divElement?.classList.value.split(' ');
+    expectedClasses.forEach((className) => {
+      // equivalee a expect( divElement?.classList.value ).toContain(className);
+      expect( divClasses ).toContain(className);
+    });
+
+  });
+
+  // comprobar que exita un link con el texto 'buy me a beer'
+  it('should contain the \'buy me a beer\' link', () => {
+    const divElements = compiled.querySelectorAll('div'); // obtiene todos primer div
+    // console.log(divElements);
+    const divElement = divElements[divElements.length-1]; // se coge el ultimo div renderizado en app.html, el que contiente un anchor bar
+    console.log(divElement)
+
+    const anchorElement = divElement?.querySelector('a')
+    expect(divElement).not.toBeNull();
+    expect( anchorElement ).not.toBeNull();
+    expect( anchorElement?.title).toContain('Buy me a beer');
+    // expect( anchorElement?.href).toBe('https://www.buymeacoffee.com/scottwindon');
+    expect( anchorElement?.getAttribute('href')).toBe('https://www.buymeacoffee.com/scottwindon');
+
+  });
 });
