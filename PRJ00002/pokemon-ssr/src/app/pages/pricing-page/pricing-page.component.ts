10a1f5b923da2d3be64bb62bcd6e520f0a6d4adc
andres.rpenuela <andresruizpenuela@gmail.com>
2025-08-13 16:28:04 +0200

    [PRJ000003] feat: se crea proyecto las paginas y se configura las rutas de la aplicación

---
+++ PRJ00002/pokemon-ssr/src/app/pages/pricing-page/pricing-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pricing-page',
+  templateUrl: './pricing-page.component.html',
+  styleUrls: ['./pricing-page.component.css']
+})
+export default class PricingPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
