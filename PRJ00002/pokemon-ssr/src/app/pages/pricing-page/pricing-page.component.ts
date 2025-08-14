3fdbd4ab722f00b9754419b2b66c6dd1e999b58c
andres.rpenuela <andresruizpenuela@gmail.com>
2025-08-14 13:58:09 +0200

    [PRJ000003] feat: uso recomendado para añadir title y metatags con ssr

--- PRJ00002/pokemon-ssr/src/app/pages/pricing-page/pricing-page.component.ts
+++ PRJ00002/pokemon-ssr/src/app/pages/pricing-page/pricing-page.component.ts
@@ -20,12 +20,13 @@ export default class PricingPageComponent implements OnInit {
 
   ngOnInit() {
     // cambia <title> del Head del Html
-    // this.title.setTitle('Pricing Page')
-    // // añade/cambia el meta
-    // this.meta.updateTag({name:'description',content: 'Este es mi Pricing Page'});
-    // this.meta.updateTag({name:'og:title',content: 'Este es mi Pricing Page'});
-    // this.meta.updateTag({name:'keywords',content: 'Hola,Andres,Mundo,Fernando,Angular,PRO'});
+    this.title.setTitle('Pricing Page')
+    // añade/cambia el meta
+    this.meta.updateTag({name:'description',content: 'Este es mi Pricing Page'});
+    this.meta.updateTag({name:'og:title',content: 'Este es mi Pricing Page'});
+    this.meta.updateTag({name:'keywords',content: 'Hola,Andres,Mundo,Fernando,Angular,PRO'});
 
+    // PLATFORM_ID - Cliente y Server
     // Da error, pero compila y funciona
     // Page reload sent to client(s). ERROR ReferenceError: document is not defined
     // document.title='Princing Page';
@@ -34,12 +35,12 @@ export default class PricingPageComponent implements OnInit {
     // pero en el navegador solo aparece una vez, que es lo que se carga una vez ejeuctado en el servidor y enviado al cliente
     //console.log({hola: 'mudno'});
 
-    console.log('estoy en el servidor? ');
-    console.log( isPlatformServer( this.platform ) );
-    if(!isPlatformServer( this.platform) ){
-      // no se ejecte en el lado del serivcdor
-      document.title='Princing Page';
-    }
+    // console.log('estoy en el servidor? ');
+    // console.log( isPlatformServer( this.platform ) );
+    // if(!isPlatformServer( this.platform) ){
+    //   // no se ejecte en el lado del serivcdor
+    //   document.title='Princing Page';
+    // }
   }
 
 }
