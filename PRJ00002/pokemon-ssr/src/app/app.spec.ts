a97b1e2ddfce75850d4097dc88a92870f15e1791
andres.rpenuela <andresruizpenuela@gmail.com>
2025-08-28 23:04:45 +0200

    [PRJ000008] feat: se elimian lostes y se apata el test de app (test basico), sin configuraion de karma, se añade la zones.js y provadier de rutas vaicas, al ser una aplicaion de angular 20 standalone ssr y que trabaja en zoneless, para establecer un punto de inicio

--- PRJ00002/pokemon-ssr/src/app/app.spec.ts
+++ PRJ00002/pokemon-ssr/src/app/app.spec.ts
@@ -1,12 +1,16 @@
 import { provideZonelessChangeDetection } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { provideRouter } from '@angular/router';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
-      providers: [provideZonelessChangeDetection()]
+      providers: [
+        //provideZonelessChangeDetection(),
+        provideRouter([]), // 👈 añade providers del router, unas rutas vacías
+      ]
     }).compileComponents();
   });
 
@@ -16,10 +20,10 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(App);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, pokemon-ssr');
-  });
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(App);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement as HTMLElement;
+  //   expect(compiled.querySelector('h1')?.textContent).toContain('Hello, pokemon-ssr');
+  // });
 });
