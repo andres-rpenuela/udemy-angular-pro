7366c1ecb48f61ea78d3e11ac725ac7f8e0bfe7b
andres.rpenuela <andresruizpenuela@gmail.com>
2025-09-28 23:13:21 +0200

    [PRJ00009] feat: instalación de tanstack query + configuarcion

--- PRJ00003/github-issues/src/app/app.config.ts
+++ PRJ00003/github-issues/src/app/app.config.ts
@@ -2,11 +2,22 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import {
+  provideTanStackQuery,
+  QueryClient
+} from '@tanstack/angular-query-experimental'
+import { withDevtools } from '@tanstack/angular-query-experimental/devtools'
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    provideTanStackQuery(
+      new QueryClient(),
+      withDevtools(() => ({ loadDevtools: 'auto' })),
+    )
   ]
 };
