21f77586c2dc08714019661b8fd28b34726e8dd9
andres.rpenuela <andresruizpenuela@gmail.com>
2025-09-28 22:59:48 +0200

    [PRJ00009] feat: se crean las paginas y se configuran las rutas basicas

---
+++ PRJ00003/github-issues/src/app/modules/issues/pages/issuses-list-page/issuses-list-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-issuses-list-page',
+  templateUrl: './issuses-list-page.component.html',
+  styleUrls: ['./issuses-list-page.component.css']
+})
+export default class IssusesListPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
